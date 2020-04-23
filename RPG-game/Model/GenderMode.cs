a6fbbba56e90eb534d836e8a7020ed82eb43b3cc
Adam Antoš <adam.antos@pslib.cz>
2020-04-23 20:33:57 +0200

    Commit 2

---
+++ RPG-game/Model/GenderMode.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace RPG_game.Model
+{
+    public enum GenderMode
+    {
+        [Display(Name = "Obojí")]
+        Both = 0,
+        [Display(Name = "Muži")]
+        Men = 1,
+        [Display(Name = "Ženy")]
+        Women = 2,
+    }
+}
---
+++ RPG-game/Model/Stats.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace RPG_game.Model
+{
+    public class Stats
+    {
+        public GenderMode PreferedGender { get; set; }
+        public string Name { get; }
+        public int DateCount { get; set; }
+        public int RelationshipLevel { get; set; }
+        public int IntelligenceLevel { get; set; }
+        public int CreativityLevel { get; set; }
+        public int HumorLevel { get; set; }
+        public int AttractionLevel { get; set; }
+    }
+}
